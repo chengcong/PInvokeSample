f2f73d779a464914093b46605ad710d66718a259
chengcong <16750571@qq.com>
2018-11-20 12:38:13 +0800

    第一次提交

---
+++ PInvokeSamples/PInvoke.Shared/PInvoke.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PInvokeSamples
+{
+    public class PInvoke
+    {
+        /// <summary>
+        /// 标准微软C运行库"msvcrt.dll"：输出文本
+        /// </summary>
+        /// <param name="msg">输出文本</param>
+        /// <returns></returns>
+        [DllImport("msvcrt.dll")]
+        public static extern int puts(string msg);
+        /// <summary>
+        /// 标准微软C运行库"msvcrt.dll":清除输出流
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("msvcrt.dll")]
+        public static extern int _flushall();
+        /// <summary>
+        /// Win32 API调用：弹出对话框
+        /// </summary>
+        /// <param name="hWnd">对话框句柄</param>
+        /// <param name="lpText">输出文本</param>
+        /// <param name="lpCaption">标题</param>
+        /// <param name="uType">对话框类型</param>
+        /// <returns></returns>
+        [DllImport("user32.dll",EntryPoint ="MessageBox")]
+        public static extern int MessageBox(int hWnd, string lpText, string lpCaption, uint uType);
+    }
+}
---
+++ PInvokeSamples/PInvokeSamples.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PInvokeSamples.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PInvoke.puts("微软标准C运行库msvcrt.dll打印的字符");
+            //PInvoke._flushall();
+            PInvoke.MessageBox(0, "AAA", "BBB", 1);
+        }
+    }
+}
